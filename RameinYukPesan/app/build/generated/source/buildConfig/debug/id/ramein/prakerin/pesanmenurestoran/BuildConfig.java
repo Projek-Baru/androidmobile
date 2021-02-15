be0051f42fa53c228600f833ae05838e2fb457e1
nadyaputriwulandari <52444673+nadyaputriwulandari@users.noreply.github.com>
2021-02-16 00:00:18 +0700

    coba

---
+++ RameinYukPesan/app/build/generated/source/buildConfig/debug/id/ramein/prakerin/pesanmenurestoran/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package id.ramein.prakerin.pesanmenurestoran;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "id.ramein.prakerin.pesanmenurestoran";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
